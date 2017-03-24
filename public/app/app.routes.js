25f1b281f1dc63e63974eb2d80c08dd41da28a55
ag251994 <ag251994@gmail.com>
2017-03-24 11:16:04 +0530

    live market updated(static data)

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -8,9 +8,12 @@ angular.module('appRoutes', ['ngRoute'])
 		.when('/', {
 			templateUrl: 'app/views/pages/appointment.html'
 		})
-		.when('/login', {
-			templateUrl: 'app/views/pages/login.html'
-		})
+    .when('/login', {
+      templateUrl: 'app/views/pages/login.html'
+    })
+    .when('/market', {
+      templateUrl: 'app/views/pages/market.html'
+    })
 
 	$locationProvider.html5Mode(true);
 
